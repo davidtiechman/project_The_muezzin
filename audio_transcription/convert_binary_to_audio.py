e77d3c0954b31a03eb4535dc323fb45b6fb55fab
Knobloch Moshe <moskno1@gmail.com>
2025-09-09 17:40:29 +0300

    I fixed step 3.

--- audio_transcription/convert_binary_to_audio.py
+++ audio_transcription/convert_binary_to_audio.py
@@ -1,11 +1,12 @@
 from logger import Logger
 
 logger = Logger.get_logger()
-def convert_binary_to_audio(binary_file,audio_file):
+def convert_binary_to_audio(doc,audio_file):
     try:
+        audio_binary = doc["binary_data"]
         with open(audio_file, 'wb') as f:
             logger.info('convert binary file to audio')
-            f.write(binary_file)
+            f.write(audio_binary)
         return audio_file
     except:
         logger.error('cannot convert binary file to audio')
